eee4a964deac5d9b2c4dcfcbc9e4c729f7961f44
thecloudtechin <arun@thecloudtech.in>
2023-02-11 00:31:10 +0530

    provisioners removed

--- terraform/modules/ec2/instance.tf
+++ terraform/modules/ec2/instance.tf
@@ -1,15 +1,15 @@
 
-resource "aws_key_pair" "dove-key" {
-  key_name = "dovekey"
-  public_key = file("terraform/modules/ec2/dove-key.pub")
+# resource "aws_key_pair" "dove-key" {
+#   key_name = "dovekey"
+#   public_key = file("terraform/modules/ec2/dove-key.pub")
   
-}
+# }
 
 resource "aws_instance" "ec2-instances" {
     ami = "ami-0f8ca728008ff5af4"
     availability_zone = "ap-south-1a"
     instance_type = "t3.medium"
-    key_name = aws_key_pair.dove-key.key_name
+    # key_name = aws_key_pair.dove-key.key_name
   #  vpc_security_group_ids = [ "sg-0e8aa9a9abb78902b" ]
     count = 1
     tags = {
@@ -18,25 +18,25 @@ resource "aws_instance" "ec2-instances" {
     }
 
 
-    provisioner "file" {
-      source = "web.sh"
-      destination = "/tmp/web.sh"
+    # provisioner "file" {
+    #   source = "web.sh"
+    #   destination = "/tmp/web.sh"
 
-    }
+    # }
 
-    provisioner "remote-exec" {
-      inline = [
-        "chmod u+x /tmp/web.sh",
-        "sudo  /tmp/web.sh "
-      ]
+    # provisioner "remote-exec" {
+    #   inline = [
+    #     "chmod u+x /tmp/web.sh",
+    #     "sudo  /tmp/web.sh "
+    #   ]
       
-    }
-    connection {
-      user = "ubuntu"
-      private_key = file(".terraform/modules/ec2/dove-key")
-      host = self.public_ip
+    # }
+    # connection {
+    #   user = "ubuntu"
+    #   private_key = file(".terraform/modules/ec2/dove-key")
+    #   host = self.public_ip
       
-    }
+    # }
   
 }
 
