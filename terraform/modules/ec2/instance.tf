8c18eae6e5a9ede378d8507b4911f15340373eed
thecloudtechin <arun@thecloudtech.in>
2023-02-11 00:10:46 +0530

    key location updated

--- terraform/modules/ec2/instance.tf
+++ terraform/modules/ec2/instance.tf
@@ -1,15 +1,15 @@
 
-# resource "aws_key_pair" "dove-key" {
-#   key_name = "dovekey"
-#   public_key = file("dove-key.pub")
+resource "aws_key_pair" "dove-key" {
+  key_name = "dovekey"
+  public_key = file("./modules/ec2/dove-key.pub")
   
-# }
+}
 
 resource "aws_instance" "ec2-instances" {
     ami = "ami-0f8ca728008ff5af4"
     availability_zone = "ap-south-1a"
     instance_type = "t3.medium"
-    # key_name = aws_key_pair.dove-key.key_name
+    key_name = aws_key_pair.dove-key.key_name
   #  vpc_security_group_ids = [ "sg-0e8aa9a9abb78902b" ]
     count = 1
     tags = {
@@ -33,7 +33,7 @@ resource "aws_instance" "ec2-instances" {
     }
     connection {
       user = "ubuntu"
-      private_key = file("dove-key")
+      private_key = file("./modules/ec2/dove-key")
       host = self.public_ip
       
     }
