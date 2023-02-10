6e28475c468917b253a8de8cbc80d7bc0c199f94
thecloudtechin <arun@thecloudtech.in>
2023-02-10 11:36:21 +0530

    instance type changed

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -8,7 +8,7 @@ resource "aws_key_pair" "dove-key" {
 resource "aws_instance" "ec2-instances" {
     ami = "ami-0be14a4fc6ac897de"
     availability_zone = "ap-south-1a"
-    instance_type = "t2.micro"
+    instance_type = "t3.medium"
     key_name = aws_key_pair.dove-key.key_name
   #  vpc_security_group_ids = [ "sg-0e8aa9a9abb78902b" ]
     count = 1
