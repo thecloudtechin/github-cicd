2d39c7099e01a575ef2e8732a1c95412aaf35d11
thecloudtechin <arun@thecloudtech.in>
2023-02-10 11:44:04 +0530

    key changed

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIA5Y2IBW7RJBSVPZ7V"
-    secret_key = "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"
+    access_key = "AKIA5Y2IBW7RLZ4KQODD"
+    secret_key = "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"
   
 }
