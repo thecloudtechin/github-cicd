57fa94a3f2326e956f40596bd4b0fd97c81625df
thecloudtechin <arun@thecloudtech.in>
2023-02-10 11:27:58 +0530

    image updated

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -9,7 +9,7 @@ variable "AMIS" {
     default = {
         us-east-2 = "ami-0cbea92f2377277a4"
         us-west-2 = "ami-083caf7916b3be0ba"
-        ap-south-1 = "ami-03907f4254994e14a"
+        ap-south-1 = "ami-0be14a4fc6ac897de"
     }
   
 }
